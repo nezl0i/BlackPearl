9cb4bb6edd77407d03789d46e88ec957ecbf8792
nezl0i <nezl0in@gmail.com>
2022-10-17 08:30:14 +0300

    Revert "Изменен requirements.txt"
    This reverts commit 9f413b61a938f7f4a971b8fe43104695319d806e.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,9 @@
 from django.contrib.auth.views import LogoutView, LoginView
-from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy, reverse
-from django.views.generic import CreateView, UpdateView
+from django.views.generic import CreateView
 
 from users.models import User
-from users.forms import LoginForm, SignUpForm, ProfileForm
+from users.forms import LoginForm, SignUpForm
 
 
 class UserLoginView(LoginView):
@@ -21,22 +20,22 @@ class UserLogoutView(LogoutView):
     template_name = 'index.html'
 
 
-class UserProfileView(UpdateView):
-    model = User
-    template_name = 'profile.html'
-    form_class = ProfileForm
-    success_url = reverse_lazy('users:profile')
-    # fields = ('username', 'email', 'first_name', 'about_me')
-
-    def get_success_url(self):
-        return reverse('users:profile', args=[self.kwargs.get('pk')])
-
-    def get_context_data(self, **kwargs):
-        context_data = super().get_context_data(**kwargs)
-        user_id = self.kwargs.get('pk')
-        user_item = get_object_or_404(User, pk=user_id)
-        context_data['title'] = 'Профиль'
-        return context_data
+# class UserProfileView(UpdateView):
+#     model = User
+#     template_name = 'profile.html'
+#     form_class = ProfileForm
+#     success_url = reverse_lazy('users:profile')
+#     # fields = ('username', 'email', 'first_name', 'about_me')
+#
+#     def get_success_url(self):
+#         return reverse('users:profile', args=[self.kwargs.get('pk')])
+#
+#     def get_context_data(self, **kwargs):
+#         context_data = super().get_context_data(**kwargs)
+#         user_id = self.kwargs.get('pk')
+#         user_item = get_object_or_404(User, pk=user_id)
+#         context_data['title'] = 'Профиль'
+#         return context_data
 
 
 class UserRegistrationView(CreateView):
